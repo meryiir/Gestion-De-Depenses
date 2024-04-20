57934f5f9c20c9a4a5eaffa245e79358bd2c9695
meryiir <mariemboujja14@gmail.com>
2024-04-20 22:12:59 +0100

    db commit

--- backend/DB/Database.js
+++ backend/DB/Database.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async (req, res) => {
 
-    const dbb = process.env.MONGO_URL;
-    const db = "mongodb+srv://mery:WzHHYynduS8EAUNZ@cluster0.5o9lanh.mongodb.net/mery"
+    const db = process.env.MONGO_URL;
+    
 
     const {connection} = await mongoose.connect(db, { useNewUrlParser: true });
 
